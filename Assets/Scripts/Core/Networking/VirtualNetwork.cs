45c2de7aadc058408818c28e1cf325e794bdde4b
Abourass <Abourassa@AssetVal.com>
2025-10-08 21:35:37 -0600

    Update namespaces in NetworkFileSystemExtensions and VirtualNetwork to include SampleOS prefix

--- Assets/Scripts/Core/Networking/VirtualNetwork.cs
+++ Assets/Scripts/Core/Networking/VirtualNetwork.cs
@@ -1,6 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
-using Core.Networking.Access;
+using SampleOS.Core.Networking.Access;
 using SampleOS.Core.Devices;
 
 namespace SampleOS.Core.Networking
